253d999981a8519894006ee795d1eeab48e982dd
moudev <rigobertomauriciomartinez@gmail.com>
2022-04-13 23:26:06 -0600

    feat: add item original text

--- index.js
+++ index.js
@@ -22,17 +22,27 @@ const listCustomLiterals = (md) => {
     if (isListItemOpen && isInlineInsideListItemOpen) {
       newTokens.push(token)
 
+      // flag to remove the 'inline' element because the text will be in the 'span' element
+      inlineToken.delete = true
+
+      // https://github.com/markdown-it/markdown-it/blob/d72c68b520cedacae7878caa92bf7fe32e3e0e6f/lib/token.js#L49
       const open = new md.Token("paragraph_open", "span", 1)
       newTokens.push(open)
 
+      const inlineTokenText = inlineToken.children
+        ? inlineToken.children[0].content
+        : ""
+
       const text = new md.Token("text", "", 0)
-      text.content = "new Token"
+      text.content = inlineTokenText
       newTokens.push(text)
 
       const close = new md.Token("paragraph_close", "span", -1)
       newTokens.push(close)
     } else {
-      newTokens.push(token)
+      if (!token.delete) {
+        newTokens.push(token)
+      }
     }
   })
   md.tokens = newTokens
